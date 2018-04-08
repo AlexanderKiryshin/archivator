3da4ec329fcc58bd084b6a4518d0b27c03747ef0
AlexanderKiryshin <alexanderKiryshin@gmail.com>
2018-04-08 16:27:55 +0300

    -

---
+++ Archiver/OutputConsole.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace archiver
+{
+    /// <summary>
+    /// Обеспечивает потокобезопасный вывод информации в консоль
+    /// </summary>
+    public static class OutputConsole
+    {
+        private static object lockObj = new object();
+        private static void Green()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        private static void Gray()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+       /* /// <summary>
+        /// Вывести в консоль информацию о хеше указанного блока
+        /// </summary>
+        /// <param name="id">номер блока</param>
+        /// <param name="hash">хеш</param>
+        public static void DisplayBlockHash(int id, byte[] hash)
+        {
+            lock (lockObj)
+            {
+                string hashString = ToString(hash);
+                Green();
+                Console.Write("Блок номер {0} ", id, " : ");
+                Gray();
+                Console.Write(hashString.ToUpper() + "\n\n");
+            }
+        }*/
+        public static string ToString(byte[] hash)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var @byte in hash)
+            {
+                builder.Append(@byte.ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+        public static void DisplayError(Exception ex)
+        {
+            lock (lockObj)
+            {
+                Green();
+                Console.Write("ОШИБКА: ");
+                Gray();
+                Console.Write(ex.Message + "\n\n");
+                Green();
+                Console.Write("Вывод стека: ");
+                Gray();
+                Console.Write(ex.StackTrace + "\n\n");
+            }
+        }
+        public static void ExitMessage()
+        {
+            lock (lockObj)
+            {
+                Gray();
+                Console.WriteLine();
+                Console.WriteLine("Работа программы завершена.Для выхода нажмите любую клавишу");
+            }
+        }
+
+        public static void StartMessage()
+        {
+            Console.WriteLine("Задать параметры работы программы можно в командной строке");
+            Console.WriteLine("Синтаксис -i источник [-s размер]");
+            Console.WriteLine("-i задает путь до входного файла,где [источник] - путь до файла");
+            Console.WriteLine("-s задает размер блока в байтах");
+            Console.WriteLine("Завершить программу досрочно можно комбинацией клавиш Ctrl+C");
+            Green();
+            Console.WriteLine("Для начала работы нажмите любую клавишу");
+            Gray();
+        }
+    }
+}
+
---
+++ Archiver/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace archiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputConsole.StartMessage();
+            Console.ReadKey();
+            Console.WriteLine();
+          //  Options options = new Options();
+            try
+            {
+            //    CommandLine.Parser.Default.ParseArguments(args, options);
+            }
+            catch (Exception ex)
+            {
+                OutputConsole.DisplayError(ex);
+                OutputConsole.ExitMessage();
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
